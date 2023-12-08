61ba871cb156a78c91c9eac2ebc97d732dfc2e29
Anuj Kumar <anujk7.dev@gmail.com>
2023-12-08 23:43:20 +0530

    committed

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ dotenv.config({
 
 connectDB()
 .then(()=>{
-    app.listen(process.env.port || 8000,()=>{
+    app.listen(process.env.PORT || 8000,()=>{
         console.log(`App is listening on port ${process.env.PORT}`)
     })
 })
