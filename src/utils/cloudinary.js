cdd4c6fe6679ce954631a27223a0a08585be4f5d
Anuj Kumar <anujk7.dev@gmail.com>
2023-12-11 10:31:43 +0530

    commit

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,8 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs"
+          
+cloudinary.config({ 
+  cloud_name: 'dz5m43a1r', 
+  api_key: '711561536172285', 
+  api_secret: 'iEPd8nD2jTn_19k3UTiqdxj78Hw' 
+});
